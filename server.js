0f24e4a03833aa9910f631be9167b866ccd913e0
ioan-ghisoi-cko <ioan.ghisoi@checkout.com>
2018-11-26 17:14:28 +0000

    Environment

---
+++ server.js
@@ -0,0 +1,84 @@
+var express = require("express"),
+  app = express(),
+  http = require("http"),
+  server = http.createServer(app);
+var rp = require('request-promise');
+var cors = require('cors');
+var bodyParser = require('body-parser');
+var port = process.env.PORT || 8000;
+
+app.use(cors({ origin: '*' }));
+app.use(bodyParser());
+
+app.post('/csv', function (req, res) {
+  var options = {
+    method: 'get',
+    json: true,
+    url: "https://api.checkout.com/reconciliation/transactions/download?from=" + req.body.startDate + "&to=" + req.body.endDate + "&payout_id=" + req.body.payoutId,
+    headers: {
+      'authorization': process.env.KEY,
+    }
+  }
+
+  rp(options)
+    .then(function (response) {
+      try {
+        res.status(200).send(response);
+      } catch (err) {
+        res.status(500).send("Server error");
+      }
+    })
+    .catch(function (err) {
+      res.send(err.error);
+    });
+});
+
+app.post('/balance', function (req, res) {
+  var options = {
+    method: 'get',
+    json: true,
+    url: "https://api.checkout.com/reconciliation/balance?from=" + req.body.startDate + "&to=" + req.body.endDate,
+    headers: {
+      'authorization': process.env.KEY,
+    }
+  }
+
+  rp(options)
+    .then(function (response) {
+      try {
+        res.status(200).send(response);
+      } catch (err) {
+        res.status(500).send("Server error");
+      }
+    })
+    .catch(function (err) {
+      res.send(err.error);
+    });
+});
+
+app.post('/statements', function (req, res) {
+  var options = {
+    method: 'get',
+    json: true,
+    url: "https://api.checkout.com/reconciliation/statements?from=" + req.body.startDate + "&to=" + req.body.endDate,
+    headers: {
+      'authorization': process.env.KEY,
+    }
+  }
+
+  rp(options)
+    .then(function (response) {
+      try {
+        res.status(200).send(response);
+      } catch (err) {
+        res.status(500).send("Server error");
+      }
+    })
+    .catch(function (err) {
+      res.send(err.error);
+    });
+});
+
+server.listen(port, function () {
+  console.log('Listening on port ' + port + '...')
+})
