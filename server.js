8a4138e6abf229a2ee8e9690ae8a907e3445f0e7
ioan-ghisoi-cko <ioan.ghisoi@checkout.com>
2018-11-28 11:45:53 +0000

    Environment

--- server.js
+++ server.js
@@ -11,72 +11,84 @@ app.use(cors({ origin: '*' }));
 app.use(bodyParser());
 
 app.post('/csv', function (req, res) {
-  var options = {
-    method: 'get',
-    json: true,
-    url: "https://api.checkout.com/reconciliation/transactions/download?from=" + req.body.startDate + "&to=" + req.body.endDate + "&payout_id=" + req.body.payoutId,
-    headers: {
-      'authorization': process.env.KEY,
-    }
-  }
-
-  rp(options)
-    .then(function (response) {
-      try {
-        res.status(200).send(response);
-      } catch (err) {
-        res.status(500).send("Server error");
+  if (process.env.PK == req.body.pk) {
+    var options = {
+      method: 'get',
+      json: true,
+      url: "https://api.checkout.com/reconciliation/transactions/download?from=" + req.body.startDate + "&to=" + req.body.endDate + "&payout_id=" + req.body.payoutId,
+      headers: {
+        'authorization': process.env.KEY,
       }
-    })
-    .catch(function (err) {
-      res.send(err.error);
-    });
+    }
+
+    rp(options)
+      .then(function (response) {
+        try {
+          res.status(200).send(response);
+        } catch (err) {
+          res.status(500).send("Server error");
+        }
+      })
+      .catch(function (err) {
+        res.send(err.error);
+      });
+  } else {
+    res.status(500).send("Server error");
+  }
 });
 
 app.post('/balance', function (req, res) {
-  var options = {
-    method: 'get',
-    json: true,
-    url: "https://api.checkout.com/reconciliation/balance?from=" + req.body.startDate + "&to=" + req.body.endDate,
-    headers: {
-      'authorization': process.env.KEY,
-    }
-  }
-
-  rp(options)
-    .then(function (response) {
-      try {
-        res.status(200).send(response);
-      } catch (err) {
-        res.status(500).send("Server error");
+  if (process.env.PK == req.body.pk) {
+    var options = {
+      method: 'get',
+      json: true,
+      url: "https://api.checkout.com/reconciliation/balance?from=" + req.body.startDate + "&to=" + req.body.endDate,
+      headers: {
+        'authorization': process.env.KEY,
       }
-    })
-    .catch(function (err) {
-      res.send(err.error);
-    });
+    }
+
+    rp(options)
+      .then(function (response) {
+        try {
+          res.status(200).send(response);
+        } catch (err) {
+          res.status(500).send("Server error");
+        }
+      })
+      .catch(function (err) {
+        res.send(err.error);
+      });
+  } else {
+    res.status(500).send("Server error");
+  }
 });
 
 app.post('/statements', function (req, res) {
-  var options = {
-    method: 'get',
-    json: true,
-    url: "https://api.checkout.com/reconciliation/statements?from=" + req.body.startDate + "&to=" + req.body.endDate,
-    headers: {
-      'authorization': process.env.KEY,
-    }
-  }
-
-  rp(options)
-    .then(function (response) {
-      try {
-        res.status(200).send(response);
-      } catch (err) {
-        res.status(500).send("Server error");
+  if (process.env.PK == req.body.pk) {
+    var options = {
+      method: 'get',
+      json: true,
+      url: "https://api.checkout.com/reconciliation/statements?from=" + req.body.startDate + "&to=" + req.body.endDate,
+      headers: {
+        'authorization': process.env.KEY,
       }
-    })
-    .catch(function (err) {
-      res.send(err.error);
-    });
+    }
+
+    rp(options)
+      .then(function (response) {
+        try {
+          res.status(200).send(response);
+        } catch (err) {
+          res.status(500).send("Server error");
+        }
+      })
+      .catch(function (err) {
+        res.send(err.error);
+      });
+  } else {
+    res.status(500).send("Server error");
+  }
 });
 
 server.listen(port, function () {
